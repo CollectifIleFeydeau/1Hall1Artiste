76a5a1722961d21c5777db9e81c7fde08411463c
julienfritsch44 <50584759+julienfritsch44@users.noreply.github.com>
2025-05-24 09:48:59 +0200

    Ajout du fichier de types pour les achievements

---
+++ src/types/achievements.ts
@@ -0,0 +1,19 @@
+/**
+ * Types pour le système d'achievements
+ */
+
+export enum AchievementType {
+  FIRST_EVENT_SAVED = 'FIRST_EVENT_SAVED',
+  FIVE_EVENTS_SAVED = 'FIVE_EVENTS_SAVED',
+  ALL_LOCATIONS_VISITED = 'ALL_LOCATIONS_VISITED',
+  FIRST_LOCATION_VISITED = 'FIRST_LOCATION_VISITED',
+  HALF_LOCATIONS_VISITED = 'HALF_LOCATIONS_VISITED'
+}
+
+export interface Achievement {
+  id: AchievementType;
+  title: string;
+  description: string;
+  unlocked: boolean;
+  unlockedAt?: Date;
+}
