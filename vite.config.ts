c7c3f2ba2b7004069b8e6e2646cdb72ad85f652f
julienfritsch44 <julien.fritsch@mariadb.com>
2025-10-17 14:58:16 +0200

    chore: remove lovable-tagger package and its usage in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -46,8 +45,6 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
